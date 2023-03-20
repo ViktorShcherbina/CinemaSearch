c3378679003824d6582a20f879da6722e196472d
ViktorShcherbina <viteyk-86@mail.ru>
2023-03-20 22:37:11 +0500

    creation activity

---
+++ app/src/main/java/store/devshcherbinavv/cinemasearch/MainActivity.kt
@@ -0,0 +1,11 @@
+package store.devshcherbinavv.cinemasearch
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
