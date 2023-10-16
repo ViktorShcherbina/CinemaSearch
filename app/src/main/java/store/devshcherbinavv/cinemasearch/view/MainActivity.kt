33bda3b0ac0e121a5864c01cc6b3a3028bf55b0e
ViktorShcherbina <viteyk-86@mail.ru>
2023-10-17 00:48:11 +0500

    add BroadcastReceiver

--- app/src/main/java/store/devshcherbinavv/cinemasearch/view/MainActivity.kt
+++ app/src/main/java/store/devshcherbinavv/cinemasearch/view/MainActivity.kt
@@ -1,5 +1,8 @@
 package store.devshcherbinavv.cinemasearch.view
 
+import android.content.BroadcastReceiver
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
@@ -8,6 +11,7 @@ import androidx.fragment.app.Fragment
 import store.devshcherbinavv.cinemasearch.R
 import store.devshcherbinavv.cinemasearch.databinding.ActivityMainBinding
 import store.devshcherbinavv.cinemasearch.data.entity.Film
+import store.devshcherbinavv.cinemasearch.receivers.ConnectionChecker
 import store.devshcherbinavv.cinemasearch.view.*
 import store.devshcherbinavv.cinemasearch.view.fragments.*
 
@@ -16,6 +20,7 @@ import store.devshcherbinavv.cinemasearch.view.fragments.*
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var receiver: BroadcastReceiver
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,8 +34,20 @@ class MainActivity : AppCompatActivity() {
             .add(R.id.fragment_placeholder, HomeFragment())
             .addToBackStack(null)
             .commit()
+        receiver = ConnectionChecker()
+        val filters = IntentFilter().apply {
+            addAction(Intent.ACTION_POWER_CONNECTED)
+            addAction(Intent.ACTION_BATTERY_LOW)
+            addAction(Intent.ACTION_AIRPLANE_MODE_CHANGED)
+        }
+        registerReceiver(receiver, filters)
 
    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(receiver)
+    }
 
     fun launchDetailsFragment(film: Film) {
         val bundle = Bundle()
@@ -121,6 +138,7 @@ class MainActivity : AppCompatActivity() {
             .replace(R.id.fragment_placeholder, fragment, tag)
             .addToBackStack(null)
             .commit()
+
     }
 
 }
